943b72ec70f2d3ddaf7e3a79d4afaca8666064b5
redninjasir <knightredninja@hotmail.com>
2017-05-01 21:24:30 +0700

    Add files via upload

--- csc105/src/evilhangman/Dictionary.java
+++ csc105/src/evilhangman/Dictionary.java
@@ -5,20 +5,57 @@
  */
 package evilhangman;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author redninjasir
  */
 public class Dictionary {
-    HashMap map;
+
+    HashMap<Integer, String> dmap;
     String Familykey;
-    public void Dictionary(String file){
-    
+    File file;
+    int count;
+    int count1;
+    String word;
+    String newWord;
+
+    public Dictionary(String file) {
+        this.dmap = new HashMap<>();
+        this.file = new File("dictionary.txt");
+        this.count = 0;
+        this.count1 = 0;
     }
-   public String format(String word, char ch, String CurrendFk){
-       return "asdf";
-   }
-   
+
+    public void ReadWord() {
+        try {
+            Scanner sc = new Scanner(new File("dictionary.txt"));
+            while (sc.hasNext()) {
+                String sci = sc.next().trim().toLowerCase();
+                dmap.put(sci.length(), sci);
+            }
+            /*if(dmap.values().toString().length() == ranlength){
+               List<String> value = new ArrayList<>(dmap.values());
+            }*/
+            List<Integer> key = new ArrayList<>(dmap.keySet());
+            int randomIn = new Random().nextInt(key.size());
+            word = dmap.get(randomIn);
+            //int wordlength = word.length();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Dictionary.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String getWord() {
+        return word;
+    }
 }
--- csc105/src/evilhangman/hangman.java
+++ csc105/src/evilhangman/hangman.java
@@ -5,6 +5,8 @@
  */
 package evilhangman;
 
+import java.util.Scanner;
+
 /**
  *
  * @author redninjasir
@@ -14,102 +16,106 @@ public class hangman {
     Dictionary Dic;
     String showWords;
     int wordlength;
-    String[] guesssLetters;
-    String dashLine;
+    char[] guessLetters;
+    char[] dashLine;
     int guessCount;
     boolean gameResult;
     String ChosenWord;
+    Scanner sc = new Scanner(System.in);
+    int guessRight;
 
-    public void hangman() {
+    public hangman() {
+        this.Dic = new Dictionary("dictionary.txt");
+        Dic.ReadWord();
+        this.wordlength = Dic.word.length();
+        this.showWords = Dic.getWord();
+        this.guessLetters = new char[20];
+        this.dashLine = new char[wordlength];
+        this.guessCount = 0;
+        this.guessRight = 0;
     }
 
     public void setWordlength(int wordlength) {
         this.wordlength = wordlength;
     }
-
-    public int getWordlength() {
-        return wordlength;
-    }
-
-    public int getGuessCount() {
-        return guessCount;
-    }
-
-    public boolean getGameResult() {
-        return gameResult;
-    }
-
     public String getChosenWord() {
-        return ChosenWord;
+        return showWords;
     }
 
-    public String[] setGuessLetter(String ch) {//ch : char
-        String[] a = {" "};
-        String[] b = {"Error"};
-        if (ch.length() < 1) {
-            if (ch.equals(" ") == false) {
-                guesssLetters[guessCount] = ch;
-                guessCount++;
-                return a;
-            } else {
-                return b;
-            }
+    public void inputGuessLetter(char ch) {//char[]
+        if (!new String(guessLetters).contains(String.valueOf(ch))) {
+            isLetterRightGuess(ch);
+            this.guessLetters[guessCount] = ch;
+            guessCount++;
+            isLetterRightGuess(ch);
+            displayDashLine();
         } else {
-            return b;
+            System.out.println("Error pls enter again");
+            inputGuessLetter(sc.next().charAt(0));
+            this.guessLetters[guessCount] = ch;
+            guessCount--;
+            guessCount++;
         }
     }
 
-    public boolean isGuessletterRepeated() {
+    public boolean isLetterRightGuess(char ch) {
         boolean check = false;
-        for(int i = 0;i<guesssLetters.length;i++){
-            if(guesssLetters[i].equals(guesssLetters[guessCount]))
-                check = false;
-            else
-                check = true;
+        for (int j = 0; j < showWords.length(); j++) {
+            if ((ch) == showWords.charAt(j)) {
+                dashLine[j] = ch;
+            }
         }
         return check;
     }
-    public boolean isLetterRightGuess() {
-        return true;
+
+    public void setChosenWord() {
+        this.showWords = Dic.getWord();
     }
 
-    public boolean selectWords() {
-        return false;
-    }
-
-    public Dictionary WordListEliminating(Dictionary Dic) {//Dic : Dictionary
-        return Dic;
-    }
-
-    public int setGuessCount() {
-
-        return guessCount;
-    }
-
-    public boolean setGameResult() {
-        return gameResult;
-    }
-
-    public String setChosenWord() {
-        return ChosenWord;
-    }
-
-    public String displayDashLine() {
-        return dashLine;
-    }
-
-    public String[] guessesLetters(String ch) { //ch : char
-        return guesssLetters;
-    }
-
-    public void displayGuessesLetters(String letter) {
-        for (int i = 0; i < guesssLetters.length; i++) {
-            letter = guesssLetters[i] + "";
+    public void setDashLine() {
+        for (int j = 0; j < dashLine.length; j++) {
+            dashLine[j] = '_';
         }
-        System.out.println(letter);
     }
 
-    public int Remainningwords() {
-        return wordlength;
+    public void displayDashLine() {
+        for (int j = 0; j < dashLine.length; j++) {
+            System.out.print(dashLine[j] + " ");
+        }
+        System.out.println();
+    }
+
+    public char[] guessesLetters(char ch) { //ch : char
+        return guessLetters;
+    }
+
+    public void displayGuessesLetters() {
+        for (int i = 0; i < guessCount; i++) {
+            System.out.print(guessLetters[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void play() {
+        System.out.println("Welcome to evil hangman");
+        setDashLine();
+        displayDashLine();
+        System.out.println("Wordlength is "+wordlength);
+        while (guessCount < 15) {
+            char ch = sc.next().toLowerCase().charAt(0);
+            inputGuessLetter(ch);
+            System.out.print("Guessed letter: ");
+            displayGuessesLetters();
+            if (!(""+dashLine).contains("_")) {
+                if (guessCount == 14) {
+                    System.out.println("--------------Lose---------------");
+                    System.out.println(getChosenWord());
+                    break;
+                }
+            } else {
+                System.out.println("--------------Win----------------");
+                break;
+            }
+        }
     }
 }
